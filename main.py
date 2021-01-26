ac9ab0059693f877fb5ef1591949f295de173090
Auhbon Amiri <auhbon@gmail.com>
2021-01-26 23:57:10 +0000

    added care package command

--- main.py
+++ main.py
@@ -2,8 +2,17 @@ import discord
 import os
 import requests
 import json
+import random
 
 client = discord.Client()
+
+gr0 = ["Hello ", "Howdy ", "Hey ", "Greetings ", "Salutations ", "Whatup ", "So nice to hear from you ", "It's a pleasure to see you "]
+
+gr1 = ["my lovely little lemon drop!!", "Carven!!", "my little hamster!!", "my precious angel!!", "my beautiful girl!!", "Howdy my special cutie pie!!", "my super duper peepee pooper!!"]
+
+gr2 = [" :) ", " :D ", " ^-^ ", " c: ", " :> ", "  :] "]
+
+gr3 = ["How may I serve you today?", "What's going on?", "How can I help?", "What would you like?"]
 
 def get_hug():
   response = requests.get("https://some-random-api.ml/animu/hug")
@@ -29,28 +38,34 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+  msg = message.content.lower()
+
   if message.author == client.user:
     return
   
-  if message.content.startswith('!hi') or message.content.startswith('!hello'):
+  if msg.startswith('hi fifi') or msg.startswith('hello fifi'):
     pat = get_pat()
-    await message.channel.send(pat + " Hello my lovely little lemon drop!! How may I serve you today? :)")
+    await message.channel.send(pat+" "+random.choice(gr0)+random.choice(gr1)+random.choice(gr2)+random.choice(gr3))
 
-  if message.content.startswith('!hug'):
+  if msg.startswith('carven want hug'):
     hug = get_hug()
     await message.channel.send(hug)
 
-  if message.content.startswith('!meme'):
+  if msg.startswith('carven want meme'):
     meme = get_meme()
     await message.channel.send(meme)
 
+  if msg.startswith('carven want package'):
+    mega = os.getenv('MEGA')
+    await message.channel.send(mega)
+    
 client.run(os.getenv('TOKEN'))
 
-#I'm thinking that I might just download a bunch of images instead of hitting an api to produce them... not sure.
+#I'm thinking that I might just download a bunch of images instead of hitting an api to produce them... not sure. (for hamsters specifically)
 
 ##TODO:
-#!info - lists commands and functionality 
-#!help - are you having anxiety? -> gives advice to calm down 
+#(last)!info - lists commands and functionality 
+#(ready)!help - are you having anxiety? -> gives advice to calm down 
 #baby bubba?
 #!wisdom - sends zen/stoic quotes
 #!package - sends emergency care package
