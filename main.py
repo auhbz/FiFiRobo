c7bc3fee26352d7616b20b8a49afe9f368ab219f
Auhbon Amiri <auhbon@gmail.com>
2021-02-26 01:50:54 +0000

    Added wip messages for the video and memory commands. Updated the help command. Updated the info command.

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import random
 
 client = discord.Client()
 
+wipmsg = "Whoopsie, this is a wip! But in the future, this will "
+
 gr0 = ["Hello ", "Howdy ", "Hey ", "Greetings ", "Salutations ", "Whatup ", "So nice to hear from you ", "It's a pleasure to see you "]
 
 gr1 = ["my lovely little lemon drop!!", "Carven!!", "my little hamster!!", "my precious angel!!", "my beautiful girl!!", "Howdy my special cutie pie!!", "my super duper peepee pooper!!", "baby bubba!!"]
@@ -31,6 +33,12 @@ def get_meme():
   json_data = json.loads(response.text)
   meme = json_data['url']
   return meme
+
+def get_video():
+  return wipmsg+"return a random video from the special youtube playlist i made for carven!"
+
+def get_memory():
+  return wipmsg+"return a random happy memory that carven and auhbon shared!"
 
 @client.event
 async def on_ready():
@@ -60,11 +68,19 @@ async def on_message(message):
     mega = os.getenv('MEGA')
     await message.channel.send(mega)
   
+  if msg.startswith('carven want video'):
+    video = get_video()
+    await message.channel.send(video)
+
+  if msg.startswith('carven want memory'):
+    memory = get_memory()
+    await message.channel.send(memory)
+
   if msg.startswith('carven want help'):
-    await message.channel.send("LOVE!!! Are you having anxiety?Is your chest hurting? Whatever is going on, I'm here to help! :D\n\n`1) focus on your breathing`\n`2) realize this will pass and if theres pain it is likely due to stress`\n`3) its all in your head, you can control it`\n`4) Focus. On. Your. Breathing`\n`5) look at hamsters, potato trump, memes`\n`6) find my hoody and cuddle with it 7) stay hydrated!`\n\nDon't forget to use my commands to help you out if needed :)\n\n**Finally, Remember:**\nThis bot itself is something i made out of love and care for you. Even though I might not always be at my best, this bot will... even if i might not be around or awake all the time, this bot will... let it be a happy thing, a comforting thing, a reminder of the love and care i feel for you -Auhbon")
+    await message.channel.send("LOVE!!! Are you having anxiety?Is your chest hurting? Whatever is going on, I'm here to help! :D\n\n`1) focus on your breathing`\n`2) realize this will pass and if theres pain it is likely due to stress`\n`3) its all in your head, you can control it`\n`4) Focus. On. Your. Breathing`\n`5) look at hamsters, potato trump, memes`\n`6) find my hoody and cuddle with it \n7) stay hydrated!`\n`19)Repeat steps 1-7!!`\n\nDon't forget to use my commands to help you out further as needed :)\n\n**Finally, Remember:**\nThis bot itself is something i made out of love and care for you. Even though I might not always be at my best, this bot will... even if i might not be around or awake all the time, this bot will... let it be a happy thing, a comforting thing, a reminder of the love and care i feel for you -Auhbon <3")
   
   if msg.startswith("carven want info"):
-    info_msg = "`carven want ___` : This is how you give me commands!\n\nHere are some words to try!\n`hug`\n`meme`\n`package`\n\nTry them out to see what they do! :)\n\nOne special command that you should understand ahead of time is `help`. Use this when you're having anxiety, chest pain, or are otherwise freaking out and can't get ahold of auhbz <3"
+    info_msg = "`carven want ___` : This is how you give me commands!\n\nHere are some words to try!\n`hug`\n`meme`\n`package`\n`video`\n`memory`\n\nTry them out to see what they do! :)\n\nOne special command that you should understand ahead of time is `help`. Use this when you're having anxiety, chest pain, or are otherwise freaking out and can't get ahold of auhbz <3"
     embed = discord.Embed(color = 0x00ff00)
     embed.title = "Info"
     embed.description = info_msg
