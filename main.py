8384aa0c0ca1f5b92e014bbfe65b764c45ee78f6
Auhbon Amiri <auhbon@gmail.com>
2021-03-31 19:22:03 +0000

    added the mochi command using the tenor API

--- main.py
+++ main.py
@@ -3,8 +3,14 @@ import os
 import requests
 import json
 import random
+from keepalive import keep_alive
 
 client = discord.Client()
+
+mega = os.getenv('MEGA')
+tenor = os.getenv("TENOR")
+cmd = 'carven want '
+gif_lmt = 8
 
 wipmsg = "Whoopsie, this is a wip! But in the future, this will "
 
@@ -46,6 +52,13 @@ def get_pic():
 def get_wisdom():
   return wipmsg+"return a random zen or stoic quote!"
 
+def get_mochi(search_term, index):
+  response = requests.get("https://api.tenor.com/v1/search?q=%s&key=%s&limit=%s" % (search_term, tenor, gif_lmt))
+ 
+  top_8gifs = json.loads(response.content)         
+  url = top_8gifs['results'][index]['media'][0]['gif']['url']
+  return url
+
 @client.event
 async def on_ready():
   print('We are ready, user {0.user}'.format(client))
@@ -62,42 +75,48 @@ async def on_message(message):
     await message.channel.send(pat+" "+random.choice(gr0)+random.choice(gr1)+random.choice(gr2)+random.choice(gr3))
     await message.channel.send("\n\n**use the 'carven want info' command to learn what I can do!**")
 
-  if msg.startswith('carven want hug'):
+  if msg.startswith(cmd+'hug'):
     hug = get_hug()
     await message.channel.send(hug)
 
-  if msg.startswith('carven want meme'):
+  if msg.startswith(cmd+'meme'):
     meme = get_meme()
     await message.channel.send(meme)
 
-  if msg.startswith('carven want package'):
-    mega = os.getenv('MEGA')
+  if msg.startswith(cmd+'package'):    
     await message.channel.send(mega)
   
-  if msg.startswith('carven want video'):
+  if msg.startswith(cmd+'video'):
     video = get_video()
     await message.channel.send(video)
 
-  if msg.startswith('carven want memory'):
+  if msg.startswith(cmd+'memory'):
     memory = get_memory()
     await message.channel.send(memory)
 
-  if msg.startswith('carven want pic'):
+  if msg.startswith(cmd+'pic'):
     pic = get_pic()
     await message.channel.send(pic)
 
-  if msg.startswith('carven want wisdom'):
+  if msg.startswith(cmd+'wisdom'):
     wisdom = get_wisdom()
     await message.channel.send(wisdom)
 
-  if msg.startswith('carven want help'):
+  if msg.startswith(cmd+'mochi'):
+    search_term = msg.split(cmd+'mochi ',1)[1]
+    index = random.randint(0,7)
+    mochi = get_mochi(search_term, index)
+    await message.channel.send(mochi)
+
+  if msg.startswith(cmd+'help'):
     await message.channel.send("LOVE!!! Are you having anxiety?Is your chest hurting? Whatever is going on, I'm here to help! :D\n\n`1) focus on your breathing`\n`2) realize this will pass and if theres pain it is likely due to stress`\n`3) its all in your head, you can control it`\n`4) Focus. On. Your. Breathing`\n`5) look at hamsters, potato trump, memes`\n`6) find my hoody and cuddle with it \n7) stay hydrated!`\n`19)Repeat steps 1-7!!`\n\nDon't forget to use my commands to help you out further as needed :)\n\n**Finally, Remember:**\nThis bot itself is something i made out of love and care for you. Even though I might not always be at my best, this bot will... even if i might not be around or awake all the time, this bot will... let it be a happy thing, a comforting thing, a reminder of the love and care i feel for you -Auhbon <3")
   
-  if msg.startswith("carven want info"):
-    info_msg = "`carven want ___` : This is how you give me commands!\n\nHere are some words to try!\n`hug`\n`meme`\n`package`\n`video`\n`memory`\n\nTry them out to see what they do! :)\n\nOne special command that you should understand ahead of time is `help`. Use this when you're having anxiety, chest pain, or are otherwise freaking out and can't get ahold of auhbz <3"
+  if msg.startswith(cmd+"info"):
+    info_msg = "`carven want ___` : This is how you give me commands!\n\nHere are some words to try!\n`hug`\n`meme`\n`package`\n`video`\n`memory`\n`mochi ___`\n\nTry them out to see what they do! :)\n\nOne special command that you should understand ahead of time is `help`. Use this when you're having anxiety, chest pain, or are otherwise freaking out and can't get ahold of auhbz <3"
     embed = discord.Embed(color = 0x00ff00)
     embed.title = "Info"
     embed.description = info_msg
     await message.channel.send(embed=embed)
     
+keep_alive()
 client.run(os.getenv('TOKEN'))
